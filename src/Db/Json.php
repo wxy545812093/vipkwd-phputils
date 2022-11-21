9a8ebca1ffcf70b892cda28dba5e5cf7b245cfed
wxy545812093 <wxy545812093@126.com>
2022-11-21 13:17:08 +0800

    JsonDb supports

---
+++ src/Db/Json.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @name JsonDB
+ * @author vipkwd <service@vipkwd.com>
+ * @link https://github.com/wxy545812093/vipkwd-phputils
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @copyright The PHP-Tools
+ */
+
+declare(strict_types=1);
+
+namespace Vipkwd\Utils\Db;
+
+use \JsonDb\JsonDb\JsonDb;
+
+class Json extends JsonDb
+{
+}
